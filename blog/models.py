f40a3e62dbc6950e0ea806c711081fb5c365f363
Bartmanskiy <pavlikbartmanskiy@gmail.com>
2024-02-26 22:28:25 +0200

    Added h1 tag

--- blog/models.py
+++ blog/models.py
@@ -1,8 +1,18 @@
 from django.db import models
 
-# Create your models here.
-class Blog(models.Model):
-	title = models.CharField(max_length=300)
-	date = models.DateField()
-	text = models.CharField(max_length=300)
-	image = models.ImageField(upload_to='event_images/')
+class Post(models.Model):
+    title = models.CharField(max_length=100)
+    date = models.DateField()
+    text = models.TextField(max_length=1500)
+    image = models.ImageField(upload_to='images/')
+
+    def get_summary(self):
+        return self.text[:20]
+
+
+    def __str__(self):
+        return self.title
+
+    
+
+
